f7935ce09bc234e2d2471b75ed905960948c4165
tester911007 <60608141+tester911007@users.noreply.github.com>
2021-03-19 08:36:13 +0300

    Update spiral.py
    Решение. Заполнение матрицы по спирали

--- tester911/spiral.py
+++ tester911/spiral.py
@@ -1,3 +1,4 @@
+# put your python code here
 n = int(input())
 
 matrix = [[0 for i in range(n)] for j in range(n)]
@@ -5,13 +6,27 @@ matrix = [[0 for i in range(n)] for j in range(n)]
 y, x = 0, 0
 step = 0
 limit = n - 1
+step = 0
 for i in range(1, n ** 2 + 1):
     matrix[y][x] = i
 
-    if x < limit:
-        x += 1
-    elif y < limit:
-        y += 1
+    if step % 2 == 0:
+        if x < limit:
+            x += 1
+        elif y < limit:
+            y += 1
+        else:
+            step = 1
+            x -= 1
+    elif step % 2 == 1:
+        if x > (n - limit - 1):
+            x -= 1
+        elif y > (n - limit):
+            y -= 1
+        else:
+            step = 2
+            limit -= 1
+            x += 1
 
 for i in range(len(matrix)):
     for j in range(len(matrix[i])):
